45aacea917bd0294c078988cb1ff220496ae827c
Evan MacDonald <113189803+Evan-Prime@users.noreply.github.com>
2023-12-06 10:27:18 -0400

    Added Mimic monster model, Added Timeline Trigger Script, Added a Trigger

---
+++ Assets/Scripts/Timeline Trigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    public GameObject player;
+
+    public bool OneTimeTrigger = false; // true = this trigger will self destruct after one use.
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+
+            // only use if you want to have an event that only triggers once
+            if (OneTimeTrigger == true)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+}
